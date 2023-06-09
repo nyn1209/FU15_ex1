5440d7f7f6ea3abed7ebaefe1654c9e442879cfc
Nhi <s1292003@u-aizu.ac.jp>
2023-06-09 11:34:44 +0900

    Greetings added

--- diceGame.java
+++ diceGame.java
@@ -1,7 +1,12 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class diceGame{
     public static void main(String[] args) {
+	System.out.println("What is your name?");
+	Scanner scanner = new Scanner(System.in);
+	String name = scanner.nextLine();
+	System.out.println("Hello, " + name + "!");
 	roll();
     }
 
