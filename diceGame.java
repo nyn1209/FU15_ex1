527059bfe796c52ec81b0006fdeba6ed40fba965
Nhi <s1292003@u-aizu.ac.jp>
2023-06-09 11:17:41 +0900

    Added message capability

---
+++ diceGame.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class diceGame{
+    public static void main(String[] args) {
+	roll();
+    }
+
+    public static void roll() {
+	System.out.println("Rolling dice...");
+	Random random = new Random();
+
+	int die1 = random.nextInt(6) + 1;
+	int die2 = random.nextInt(6) + 1;
+
+	System.out.println("Die 1: " + die1);
+	System.out.println("Die 2: " + die2);
+
+	int totalVal = die1 + die2;
+	System.out.println("Total value: " + totalVal);
+    }
+}
